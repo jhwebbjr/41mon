76b149923382c2c81a12444036fb3fd9e6a3c172
James Webb <webbfamily@Jamess-MacBook-Pro.local>
2017-03-07 09:16:32 -0500

    sunrise

--- httparty.rb
+++ httparty.rb
@@ -2,7 +2,7 @@ require 'httparty'
 require 'pry'
 
 class WeatherExchange
-  def questions
-    HTTParty.get('http://api.wunderground.com/api/07b67810d9df27fb/forecast/geolookup/conditions/q/CA/60619.json')
+  def conditions
+    HTTParty.get('http://api.wunderground.com/api/07b67810d9df27fb/conditions/astronomy/forecast10day/alerts/q/60619.json')
   end
 end
--- weather_test.rb
+++ weather_test.rb
@@ -3,10 +3,24 @@ require "minitest/pride"
 require "minitest/focus"
 require "webmock/minitest"
 require_relative "./httparty"
+require "pry"
 
 class WeatherExchangeTest < Minitest::Test
-  def test_can_get_most_recent_weather
-    stub_request(:get, 'http://api.wunderground.com/api/07b67810d9df27fb/conditions/q/60619.json')
-    .to_return(body: File.read("./stubbed_requests/get_questions.json"), headers: { 'Content-Type' => "application/json" })
+  def test_can_get_current_temp
+    stub_request(:get, 'http://api.wunderground.com/api/07b67810d9df27fb/conditions/astronomy/forecast10day/alerts/q/60619.json')
+    .to_return(body: File.read("./stubbed_requests/conditions_for_60619.json"), headers: { 'Content-Type' => "application/json" })
+
+    exchange = WeatherExchange.new
+    assert_equal "57.0 F (13.9 C)", exchange.conditions["current_observation"]["temperature_string"]
   end
+focus
+  def test_can_get_sunrise
+    stub_request(:get, 'http://api.wunderground.com/api/07b67810d9df27fb/conditions/astronomy/forecast10day/alerts/q/60619.json')
+    .to_return(body: File.read("./stubbed_requests/conditions_for_60619.json"), headers: { 'Content-Type' => "application/json" })
+
+    exchange = WeatherExchange.new
+    assert_equal "6", exchange.conditions["moon_phase"]["sunrise"]["hour"]
+  end
+
+
 end
